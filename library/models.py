e23f685ae209f7d1c4647bf38f022ff238367158
moapochta06 <90915948+moapochta06@users.noreply.github.com>
2024-11-26 21:16:08 +0700

    added models

--- library/models.py
+++ library/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    biography = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    publication_year = models.IntegerField()
+    genre = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    publisher = models.CharField(max_length=100)
+    cover_image = models.ImageField(upload_to='covers/')
+    file = models.FileField(upload_to='books/')
+
+    class Meta:
+        unique_together = ('title', 'author', 'publication_year', 'publisher')
+
+    def __str__(self):
+        return self.title
